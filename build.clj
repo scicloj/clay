910107ba684ff179b80535b373aaccc6a88ef2ed
daslu <daniel.slutsky@gmail.com>
2022-03-28 14:11:24 +0300

    SNAPSHOT version

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [org.corfield.build :as bb]))
 
 (def lib 'org.scicloj/clay)
-(def version "0.1.0-SNAPSHOT")
+(def version "1-alhpha1-SNAPSHOT")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 
--- test/org/scicloj/clay_test.clj
+++ test/org/scicloj/clay_test.clj
@@ -1,7 +1,2 @@
 (ns org.scicloj.clay-test
-  (:require [clojure.test :refer :all]
-            [org.scicloj.clay :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (:require [clojure.test :refer :all]))
