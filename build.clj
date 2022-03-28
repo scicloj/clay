7f714d409a0101f7a0009c0f75a678f3ffd1c2f1
daslu <daniel.slutsky@gmail.com>
2022-03-28 14:23:15 +0300

    typo in version

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [org.corfield.build :as bb]))
 
 (def lib 'org.scicloj/clay)
-(def version "1-alhpha1-SNAPSHOT")
+(def version "1-alpha1-SNAPSHOT")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 
