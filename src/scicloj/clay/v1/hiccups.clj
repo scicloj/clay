f9348e4bc4aafae0d3a506e1c165a6685729ea39
daslu <daniel.slutsky@gmail.com>
2022-04-04 00:23:52 +0300

    hiccup generation (WIP)

--- src/scicloj/clay/v1/hiccups.clj
+++ src/scicloj/clay/v1/hiccups.clj
@@ -1,7 +1,7 @@
-(ns scicloj.clay.v1.hiccups)
+(ns scicloj.clay.v1.hiccups
+  (:require [hiccup.page :as page]))
 
-(defn in-iframe [hiccup]
-  [:iframe {:width       "100%"
-            :height      "500px"
-            :frameBorder "0"
-            :srcdoc      hiccup}])
+(defn in-iframe [html]
+  (format
+   "<iframe sandbox='' frameBorder='0' width='100%%' height='500px' srcdoc='%s'></iframe>"
+   (page/html5 html)))
