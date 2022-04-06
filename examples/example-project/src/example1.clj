034c7878c17b4460076f2cee194e552f59be8781
daslu <daniel.slutsky@gmail.com>
2022-04-07 00:42:15 +0300

    cleanup of example project

--- examples/example-project/src/example1.clj
+++ examples/example-project/src/example1.clj
@@ -1,17 +1,8 @@
-;; # Clay
-
 (ns intro
   (:require [scicloj.clay.v1.api :as clay]
-            [scicloj.clay.v1.tools :as tools]
-            [scicloj.kindly.v2.api :as kindly]
-            [scicloj.kindly.v2.kind :as kind]
-            [nextjournal.clerk :as clerk]))
+            [scicloj.clay.v1.tools :as tools]))
 
 (clay/start! {:tools [tools/clerk
-                      #_tools/portal]})
-
-(comment
-  (clay/restart! {:tools [tools/clerk
-                          tools/portal]}))
+                      tools/portal]})
 
 (+ 1 2)
