6a5e0fb69c11e278367737405ab4256b65ce0f09
daslu <daniel.slutsky@gmail.com>
2022-12-16 17:12:23 +0200

    version 2-alpha11

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [org.corfield.build :as bb]))
 
 (def lib 'org.scicloj/clay)
-(def version "2-alpha10")
+(def version "2-alpha11")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 
