aa2e71869d26f7a6f255d7eb346f6c28751302a7
daslu <daniel.slutsky@gmail.com>
2023-03-28 17:20:57 +0300

    updated docs

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -285,39 +285,30 @@ people-as-vectors
 
 ;; inspired by [the official MathBox.cljs tutorial](https://mathbox.mentat.org/)
 (kind/hiccup
- '(require
-   '[mathbox.core :as mathbox]
-   '[mathbox.primitives :as mb]))
+ '(let [Data (fn []
+               [mathbox.primitives/Interval
+                {:expr (fn [emit x _i t]
+                         (emit x (Math/sin (+ x t))))
+                 :width 64
+                 :channels 2}])
+        Curve (fn []
+                [:<>
+                 [Data]
+                 [mathbox.primitives/Line {:width 5
+                                           :color "#3090FF"}]])]
+    [mathbox.core/MathBox
+     {:container {:style {:height "400px" :width "100%"}}
+      :focus 3}
+     [mathbox.primitives/Camera {:position [0 0 3]
+                                 :proxy true}]
+     [mathbox.primitives/Cartesian
+      {:range [[-2 2] [-1 1]]
+       :scale [2 1]}
 
-(kind/hiccup
- '(defn Data []
-    [mathbox.primitives/Interval
-     {:expr (fn [emit x _i t]
-              (emit x (Math/sin (+ x t))))
-      :width 64
-      :channels 2}]))
-
-(kind/hiccup
- '(defn Curve []
-    [:<>
-     [Data]
-     [mathbox.primitives/Line {:width 5
-                               :color "#3090FF"}]]))
-
-(kind/hiccup
- '[mathbox.core/MathBox
-   {:container {:style {:height "400px" :width "100%"}}
-    :focus 3}
-   [mathbox.primitives/Camera {:position [0 0 3]
-                               :proxy true}]
-   [mathbox.primitives/Cartesian
-    {:range [[-2 2] [-1 1]]
-     :scale [2 1]}
-
-    [mathbox.primitives/Axis {:axis 1 :width 3 :color "black"}]
-    [mathbox.primitives/Axis {:axis 2 :width 3 :color "black"}]
-    [mathbox.primitives/Grid {:width 2 :divideX 20 :divideY 10}]
-    [Curve]]])
+      [mathbox.primitives/Axis {:axis 1 :width 3 :color "black"}]
+      [mathbox.primitives/Axis {:axis 2 :width 3 :color "black"}]
+      [mathbox.primitives/Grid {:width 2 :divideX 20 :divideY 10}]
+      [Curve]]]))
 
 
 ;; ## Delays
