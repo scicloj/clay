04c7cafee4fc688471b84d8534b3a2c2250f11dd
daslu <daniel.slutsky@gmail.com>
2023-08-07 01:45:13 +0300

    updated example project

--- examples/example-project/notebooks/example1.clj
+++ examples/example-project/notebooks/example1.clj
@@ -4,33 +4,16 @@
 
 (ns example1
   (:require [scicloj.clay.v2.api :as clay]
-            [scicloj.viz.api :as viz]
-            [tablecloth.api :as tc]))
-
-;; ## Useful commands
-
-^:kindly/hide-code?
-(comment
-  (do (clay/show-doc! "notebooks/example1.clj"
-                      {:toc? true})
-      (clay/write-html! "docs/example1.html")))
+            [scicloj.kindly.v3.kind :as kind]))
 
 ;; ## Examples
 
 (+ 1 2)
 
-(-> (let [n 99]
-      (tc/dataset {:preferred-language (for [i (range n)]
-                                         (["clojure" "clojurescript" "babashka"]
-                                          (rand-int 3)))
-                   :age (for [i (range n)]
-                          (rand-int 100))})))
+(kind/hiccup
+ [:div {:style
+        {:background-color "#eeddcc"}}
+  [:p "hello"]])
 
-(-> [{:x 1 :y 2}
-     {:x 2 :y 4}
-     {:x 3 :y 9}]
-    viz/data
-    (viz/type :point)
-    (viz/mark-size 200)
-    (viz/color :x)
-    viz/viz)
+(kind/md
+ ["hello *hello* **hello**"])
