620cf81930cfbafbef931dccf04ffc77cb731371
daslu <daniel.slutsky@gmail.com>
2023-09-23 23:58:14 +0300

    snapshot version

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [org.corfield.build :as bb]))
 
 (def lib 'org.scicloj/clay)
-(def version "2-alpha37")
+(def version "2-alpha38-SNAPSHOT")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 
