e103bb9047222dce7000104907c39ea8aed693ce
daslu <daniel.slutsky@gmail.com>
2023-10-13 13:39:17 +0300

    updated tutorial (temporarily removing some currently-unsupported examples)

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -208,7 +208,7 @@
        (URL.)
        (ImageIO/read)))
 
-clay-image
+;; clay-image
 
 ;; ### Plain data structures
 
@@ -249,6 +249,8 @@ clay-image
             [:big [:big "bye"]])})
 
 nested-structure-1
+
+
 
 ;; ### Pretty printing
 
@@ -298,8 +300,6 @@ nested-structure-1
     tc/dataset
     kind/table)
 
-
-
 ;; ### [Vega](https://vega.github.io/vega/) and [Vega-Lite](https://vega.github.io/vega-lite/)
 
 (defn vega-lite-point-plot [data]
@@ -328,184 +328,6 @@ nested-structure-1
 
 (random-vega-lite-plot 9)
 
-;; ### [Cytoscape.js](https://js.cytoscape.org/)
-
-(def cytoscape-example
-  {:elements {:nodes [{:data {:id "a" :parent "b"} :position {:x 215 :y 85}}
-                      {:data {:id "b"}}
-                      {:data {:id "c" :parent "b"} :position {:x 300 :y 85}}
-                      {:data {:id "d"} :position {:x 215 :y 175}}
-                      {:data {:id "e"}}
-                      {:data {:id "f" :parent "e"} :position {:x 300 :y 175}}]
-              :edges [{:data {:id "ad" :source "a" :target "d"}}
-                      {:data {:id "eb" :source "e" :target "b"}}]}
-   :style [{:selector "node"
-            :css {:content "data(id)"
-                  :text-valign "center"
-                  :text-halign "center"}}
-           {:selector "parent"
-            :css {:text-valign "top"
-                  :text-halign "center"}}
-           {:selector "edge"
-            :css {:curve-style "bezier"
-                  :target-arrow-shape "triangle"}}]
-   :layout {:name "preset"
-            :padding 5}})
-
-(kind/cytoscape cytoscape-example)
-
-(kind/cytoscape [cytoscape-example
-                 {:style {:height 100
-                          :width 100}}])
-
-;; ### [Apache Echarts](https://echarts.apache.org/)
-
-(kind/echarts
- {:xAxis {:data ["Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"]}
-  :yAxis {}
-  :series [{:type "bar"
-            :color ["#7F5F3F"]
-            :data [23 24 18 25 27 28 25]}]})
-
-;; ### MathBox.cljs
-
-;; inspired by [the official MathBox.cljs tutorial](https://mathbox.mentat.org/)
-(kind/hiccup
- '[(let [Data (fn []
-                [mathbox.primitives/Interval
-                 {:expr (fn [emit x _i t]
-                          (emit x (Math/sin (+ x t))))
-                  :width 64
-                  :channels 2}])
-         Curve (fn []
-                 [:<>
-                  [Data]
-                  [mathbox.primitives/Line {:width 5
-                                            :color "#3090FF"}]])
-         Main (fn []
-                [mathbox.core/MathBox
-                 {:container {:style {:height "400px" :width "100%"}}
-                  :focus 3}
-                 [mathbox.primitives/Camera {:position [0 0 3]
-                                             :proxy true}]
-                 [mathbox.primitives/Cartesian
-                  {:range [[-2 2] [-1 1]]
-                   :scale [2 1]}
-                  [mathbox.primitives/Axis {:axis 1 :width 3 :color "black"}]
-                  [mathbox.primitives/Axis {:axis 2 :width 3 :color "black"}]
-                  [mathbox.primitives/Grid {:width 2 :divideX 20 :divideY 10}]
-                  [Curve]]])
-         *state (r/atom {:open? false})]
-     (fn []
-       [:div
-        [:button {:on-click #(swap! *state update :open? not)}
-         (if (:open? @*state)
-           "close"
-           "open")]
-        (when (:open? @*state)
-          [Main])]))])
-
-;; ### plotly
-(kind/hiccup
- '[plotly
-   {:data [{:x [0 1 3 2]
-            :y [0 6 4 5]
-            :z [0 8 9 7]
-            :type :scatter3d
-            :mode :lines+markers
-            :opacity 0.5
-            :line {:width 5}
-            :marker {:size 4
-                     :colorscale :Viridis}}]}])
-
-;; ### Katex
-(kind/hiccup
- '[katex "1+x^2"])
-
-;; ### Emmy
-(kind/hiccup
- '(require '[emmy.env :as e :refer [D]]))
-
-(kind/hiccup
- [:div
-  '(-> 'x
-       ((D e/cube))
-       e/simplify
-       e/->infix)])
-
-(kind/hiccup
- '[katex
-   (-> 'x
-       ((D e/cube))
-       e/simplify
-       e/->infix)])
-
-;; ### tmdjs
-(kind/hiccup
- '(require '[tech.v3.dataset :as tmd]))
-
-(kind/hiccup
- '[(fn []
-     [:div
-      (-> {:x [1 2 3]}
-          tmd/->dataset
-          :x
-          pr-str)])])
-
-
-;; ### 3dmol
-
-;; (experimental)
-
-(kind/hiccup
- ['three-d-mol-viewer
-  {:data-pdb "2POR"}])
-
-(kind/hiccup
- ['(fn [{:keys [pdb-data]}]
-     [:div
-      {:style {:width "100%"
-               :height "500px"
-               :position "relative"}
-       :ref (fn [el]
-              (let [config (clj->js
-                            {:backgroundColor "0xffffff"})
-                    viewer (.createViewer js/$3Dmol el #_config)]
-                (.setViewStyle viewer (clj->js
-                                       {:style "outline"}))
-                (.addModelsAsFrames viewer pdb-data "pdb")
-                (.addSphere viewer (clj->js
-                                    {:center {:x 0
-                                              :y 0
-                                              :z 0}
-                                     :radius 1
-                                     :color "green"}))
-                (.zoomTo viewer)
-                (.render viewer)
-                (.zoom viewer 0.8 2000)))}
-      ;; need to keep this symbol to let Clay infer the necessary dependency
-      'three-d-mol])
-  {:pdb-data (memoized-slurp "https://files.rcsb.org/download/2POR.pdb")}])
-
-;; ### Leaflet
-
-(kind/hiccup
- ['(fn []
-     [:div
-      {:style {:width "100%"
-               :height "500px"}
-       :ref (fn [el]
-              (let [m (-> js/L
-                          (.map el)
-                          (.setView (clj->js [52.5274319 13.4004289])
-                                    19))]
-                (-> js/L
-                    (.tileLayer "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                (clj->js {:maxZoom 19
-                                          :attribution "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"}))
-                    (.addTo m))))}
-      'leaflet])])
-
 
 ;; ## Delays
 
@@ -520,31 +342,20 @@ nested-structure-1
   (+ 1 2))
 
 ;; ## Embedded Portal
-;; (WIP)
 
+(kind/portal {:x (range 3)})
 
-{:x (range 3)}
+(kind/portal
+ [(-> [:img {:height 50 :width 50
+             :src "https://clojure.org/images/clojure-logo-120b.png"}]
+      kind/hiccup)
+  (-> [:img {:height 50 :width 50
+             :src "https://raw.githubusercontent.com/djblue/portal/fbc54632adc06c6e94a3d059c858419f0063d1cf/resources/splash.svg"}]
+      kind/hiccup)])
 
-(clay/in-portal {:x (range 3)})
-
-(def my-images
-  [(-> [:img {:height 50 :width 50
-              :src "https://clojure.org/images/clojure-logo-120b.png"}]
-       kind/hiccup)
-   (-> [:img {:height 50 :width 50
-              :src "https://raw.githubusercontent.com/djblue/portal/fbc54632adc06c6e94a3d059c858419f0063d1cf/resources/splash.svg"}]
-       kind/hiccup)])
-
-my-images
-
-(clay/in-portal my-images)
-
-(def myplot
-  (random-vega-lite-plot 9))
-
-myplot
-
-(clay/in-portal myplot)
+(kind/portal
+ [(kind/hiccup [:big [:big "a plot"]])
+  (random-vega-lite-plot 9)])
 
 ;; ## Nesting kinds in Hiccup (WIP)
 
@@ -580,9 +391,10 @@ myplot
 
 ;; ## Nesting kinds in Tables (WIP)
 
-(kind/table {:column-names [:x :y]
-             :row-vectors [[(kind/md "*some text* **some more text**")
-                            (kind/code "{:x (1 2 [3 4])}")]
-                           [(tc/dataset {:x (range 3)
-                                         :y (map inc (range 3))})
-                            (random-vega-lite-plot 9)]]})
+(kind/table
+ {:column-names [:x :y]
+  :row-vectors [[(kind/md "*some text* **some more text**")
+                 (kind/code "{:x (1 2 [3 4])}")]
+                [(tc/dataset {:x (range 3)
+                              :y (map inc (range 3))})
+                 (random-vega-lite-plot 9)]]})
