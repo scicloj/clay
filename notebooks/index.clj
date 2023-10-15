a9dcd906a8cc65df3d69ce759eebf7fbd340f87e
daslu <daniel.slutsky@gmail.com>
2023-10-15 22:54:42 +0300

    added cytoscape example

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -193,13 +193,6 @@
       (pr-str (map inc numbers))])
   (vec (range 40))])
 
-
-(kind/reagent
- ['(fn [numbers]
-     [:p {:style {:background "#d4ebe9"}}
-      (pr-str (map inc numbers))])
-  (vec (range 40))])
-
 ;; From the [reagent tutorial](https://reagent-project.github.io/):
 (kind/reagent
  ['(fn []
@@ -355,6 +348,37 @@ nested-structure-1
 
 (random-vega-lite-plot 9)
 
+;; ### Cytoscape
+
+(def cytoscape-example
+  {:elements {:nodes [{:data {:id "a" :parent "b"} :position {:x 215 :y 85}}
+                      {:data {:id "b"}}
+                      {:data {:id "c" :parent "b"} :position {:x 300 :y 85}}
+                      {:data {:id "d"} :position {:x 215 :y 175}}
+                      {:data {:id "e"}}
+                      {:data {:id "f" :parent "e"} :position {:x 300 :y 175}}]
+              :edges [{:data {:id "ad" :source "a" :target "d"}}
+                      {:data {:id "eb" :source "e" :target "b"}}]}
+   :style [{:selector "node"
+            :css {:content "data(id)"
+                  :text-valign "center"
+                  :text-halign "center"}}
+           {:selector "parent"
+            :css {:text-valign "top"
+                  :text-halign "center"}}
+           {:selector "edge"
+            :css {:curve-style "bezier"
+                  :target-arrow-shape "triangle"}}]
+   :layout {:name "preset"
+            :padding 5}})
+
+(kind/cytoscape
+ cytoscape-example)
+
+(kind/cytoscape
+ [{:style {:width "100px"
+           :height "100px"}}
+  cytoscape-example])
 
 
 ;; ## Delays
