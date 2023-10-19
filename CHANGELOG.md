e6cee0b99b8a5479c3463953bbe582db592092ec
daslu <daniel.slutsky@gmail.com>
2023-10-19 21:41:09 +0300

    new config-oriented actions (WIP)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ All notable changes to this project will be documented in this file. This change
   - updated portal dep (no longer relying on an ad-hoc patched version)
 - unifying the quarto pathways in the API: one markdown generation funciton, two different actions (with/without rendering)
 - updated kindly version
+- new config-oriented actions (WIP)
 
 ## [2-alpha38] - 2023-10-03
 - path changes in light quarto book generation
--- src/scicloj/clay/v2/quarto.clj
+++ src/scicloj/clay/v2/quarto.clj
@@ -9,7 +9,9 @@
    [scicloj.clay.v2.state :as state]
    [scicloj.clay.v2.show :as show]
    [scicloj.clay.v2.item :as item]
-   [scicloj.clay.v2.util.time :as time]))
+   [scicloj.clay.v2.eval :as eval]
+   [scicloj.clay.v2.util.time :as time]
+   [clj-yaml.core :as yaml]))
 
 (defn render-quarto! [items]
   (let [md-path (path/ns->target-path "docs/" *ns* ".md")
@@ -93,3 +95,72 @@ embed-resources: true
         (->> (spit md-path)))
     (update-quarto-config! chapter-path)
     (println [:wrote md-path (time/now)])))
+
+
+(defn source-path->target-path [path]
+  (-> path
+      (string/replace #"\.clj$"
+                      "/index.md")))
+
+(defn source-path->file-ext [path]
+  (-> path
+      (string/split #"\.")
+      last))
+
+(defn ->base-book-config [{:keys [title chapter-source-paths]}]
+  {:project {:type "book"}
+   :format {:html {:theme "cosmo"}}
+   :book {:title title
+          :chapters (->> chapter-source-paths
+                         (cons "index.md")
+                         (mapv source-path->target-path))}})
+
+(defn ->main-index [{:keys [toc embed-resources title]}]
+  (str "---\n"
+       (yaml/generate-string {:format {:html {:toc toc}}
+                              :embed-resources embed-resources})
+       "\n---\n"
+       "# " title))
+
+(defn update-book! [{:keys [base-source-path
+                            chapter-source-paths
+                            base-target-path
+                            title
+                            quarto-config
+                            toc
+                            embed-resources
+                            main-index]
+                     :or {title "Book Draft"
+                          toc true
+                          embed-resources true
+                          quarto-config (->base-book-config {:title title
+                                                             :chapter-source-paths chapter-source-paths})
+                          main-index (->main-index {:toc toc
+                                                    :embed-resources embed-resources
+                                                    :title title})}}]
+  (let [config-path (str base-target-path "/_quarto.yml")
+        main-index-path (str base-target-path "/index.md")]
+    (io/make-parents config-path)
+    (->> quarto-config
+         yaml/generate-string
+         (spit config-path))
+    (prn [:created config-path])
+    (when-not (-> main-index-path io/file .exists)
+      (spit main-index-path main-index)
+      (prn [:created main-index-path])))
+  (->> chapter-source-paths
+       (map (fn [source-path]
+              (let [target-path (-> source-path
+                                    source-path->target-path)
+                    full-source-path (str base-source-path "/" source-path)
+                    full-target-path (str base-target-path "/" target-path)]
+                (io/make-parents full-target-path)
+                (case (source-path->file-ext source-path)
+                  "md" (->> full-source-path
+                            slurp
+                            (spit full-target-path))
+                  "clj" (-> full-source-path
+                            (eval/gen-doc {:title source-path})
+                            (->> (page/md @state/*state)
+                                 (spit full-target-path))))
+                (prn [:wrote full-target-path (time/now)]))))))
