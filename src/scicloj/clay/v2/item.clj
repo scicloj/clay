11edae9ba5795a128cd46db6237f1e83246d8191
daslu <daniel.slutsky@gmail.com>
2023-10-29 23:57:46 +0200

    parsing JSON on echarts item

--- src/scicloj/clay/v2/item.clj
+++ src/scicloj/clay/v2/item.clj
@@ -148,11 +148,13 @@
               [:script
                (->> spec
                     charred/write-json-str
+                    pr-str
                     (format
                      "
 {
   var myChart = echarts.init(document.currentScript.parentElement);
-  var option = %s;
+  var option_str = %s;
+  var option = JSON.parse(option_str);
   myChart.setOption(option);
 };"))]]
      :deps [:echarts]}))
