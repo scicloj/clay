498fa08f29126b51bf406040c6bb4d7a75c7b1ee
daslu <daniel.slutsky@gmail.com>
2023-10-30 22:34:46 +0200

    added Leaflet example

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -441,6 +441,36 @@ nested-structure-1
           :marker {:size 4
                    :colorscale :Viridis}}]})
 
+;; ### Leaflet
+
+;; (experimental)
+
+;; This example was adapted from [the Leaflet website](https://leafletjs.com/).
+
+(kind/reagent
+ ^{:deps [:leaflet]}
+ ['(fn []
+     [:div
+      [:div {:style {:height "200px"}
+             :ref (fn [el]
+                    (let [m (-> js/L
+                                (.map el)
+                                (.setView (clj->js [51.505 -0.09])
+                                          13))]
+                      (-> js/L
+                          (.tileLayer "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                      (clj->js
+                                       {:maxZoom 19
+                                        :attribution "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"}))
+                          (.addTo m))
+                      (-> js/L
+                          (.marker (clj->js [51.5 -0.09]))
+                          (.addTo m)
+                          (.bindPopup "A pretty CSS popup.<br> Easily customizable.")
+                          (.openPopup))))}]])])
+
+
+
 ;; ## Delays
 
 ;; Clojure Delays are a common way to define computations that do not take place immediately. The computation takes place when dereferencing the value for the first time.
