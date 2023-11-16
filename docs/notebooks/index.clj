e794f149e11f04025bd4bd8f119cea9ae3f55bd2
daslu <daniel.slutsky@gmail.com>
2023-11-16 23:05:01 +0200

    tutorial: added csv file example

--- docs/notebooks/index.clj
+++ docs/notebooks/index.clj
@@ -619,9 +619,11 @@ nested-structure-1
 (kind/vega-lite
  {:data {:url "notebooks/datasets/iris.csv"},
   :mark "rule",
-  :encoding {:x {:field "sepal_width", :type "quantitative"},
-             :y {:field "sepal_length", :type "quantitative"},
-             :x2 {:field "petal_width", :type "quantitative"},
+  :encoding {:opacity {:value 0.2}
+             :size {:value 3}
+             :x {:field "sepal_width", :type "quantitative"},
+             :x2 {:field "sepal_length", :type "quantitative"},
+             :y {:field "petal_width", :type "quantitative"},
              :y2 {:field "petal_length", :type "quantitative"},
              :color {:field "species", :type "nominal"}}
   :background "floralwhite"})
--- notebooks/index.clj
+++ notebooks/index.clj
@@ -616,6 +616,18 @@ nested-structure-1
 (kind/hiccup
  [:img {:src "notebooks/images/Clay.svg.png"}])
 
+(kind/vega-lite
+ {:data {:url "notebooks/datasets/iris.csv"},
+  :mark "rule",
+  :encoding {:opacity {:value 0.2}
+             :size {:value 3}
+             :x {:field "sepal_width", :type "quantitative"},
+             :x2 {:field "sepal_length", :type "quantitative"},
+             :y {:field "petal_width", :type "quantitative"},
+             :y2 {:field "petal_length", :type "quantitative"},
+             :color {:field "species", :type "nominal"}}
+  :background "floralwhite"})
+
 ;; ## Coming soon
 
 ;; In the past, Clay used to support various data visualization libraries such as MathBox and KaTeX.
