b3ec8037109a8c188913a8fedba5f4721c3bc0fc
daslu <daniel.slutsky@gmail.com>
2023-11-16 17:01:19 +0200

    changed the target path stucture

--- src/scicloj/clay/v2/util/path.clj
+++ src/scicloj/clay/v2/util/path.clj
@@ -66,13 +66,18 @@
 
 (defn ns->target-path
   ([base the-ns ext]
-   (let [ns-parts (-> the-ns
-                      str
-                      (string/replace #"-" "_")
-                      (string/split #"\."))]
-     (str base
-          (string/join "/" ns-parts)
-          (if (= (last ns-parts) "index")
-            ""
-            "/index")
-          ext))))
+   (str base
+        (-> the-ns
+            str
+            (string/replace #"-" "_"))
+        ext)
+   #_(let [ns-parts (-> the-ns
+                        str
+                        (string/replace #"-" "_")
+                        (string/split #"\."))]
+       (str base
+            (string/join "/" ns-parts)
+            (if (= (last ns-parts) "index")
+              ""
+              "/index")
+            ext))))
