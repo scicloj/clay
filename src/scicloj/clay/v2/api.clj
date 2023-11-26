a663daf8479bbf0d1689e054076aa32dffadf4a4
daslu <daniel.slutsky@gmail.com>
2023-11-26 15:36:48 +0200

    moved example code back under the `make` ns

--- src/scicloj/clay/v2/api.clj
+++ src/scicloj/clay/v2/api.clj
@@ -22,7 +22,6 @@
 
 (defn browse! []
   (server/browse!))
-
 (defn port []
   (server/port))
 
@@ -34,41 +33,3 @@
 
 (defn update-book! [options]
   (quarto/update-book! options))
-
-
-(comment
-  (make! {:format [:html]
-          :source-path "notebooks/index.clj"})
-
-  (make! {:format [:html]
-          :source-path "notebooks/index.clj"
-          :show false})
-
-  ;; TODO: support a book with multiple chapters
-  ;; Quarto: create 1 book (this is just our normal configuration, but Clay can help)
-  (make! {:format [:quarto :book]
-          ;; Clay: create 3 markdown files from source
-          :source-path ["notebooks/index.clj"
-                        "notebooks/index1.clj"
-                        "notebooks/index2.clj"]
-          :show false})
-
-  (make! {:format      [:html]
-          :source-path "notebooks/index.clj"
-          :single-form '(kind/cytoscape
-                          [{:style {:width "300px"
-                                    :height "300px"}}
-                           cytoscape-example])})
-
-  (make! {:format [:quarto :html]
-          :source-path "notebooks/index.clj"})
-
-  (make! {:format [:quarto :html]
-          :source-path "notebooks/slides.clj"})
-
-  (make! {:format [:quarto :revealjs]
-          :source-path "notebooks/slides.clj"})
-
-  (make! {:format [:quarto :html]
-          :source-path "notebooks/index.clj"
-          :quarto {:highlight-style :nord}}))
--- src/scicloj/clay/v2/make.clj
+++ src/scicloj/clay/v2/make.clj
@@ -86,3 +86,41 @@
                       (merge {:html-path target-path})
                       server/update-page!))))
     [:wrote target-path]))
+
+
+(comment
+  (make! {:format [:html]
+          :source-path "notebooks/index.clj"})
+
+  (make! {:format [:html]
+          :source-path "notebooks/index.clj"
+          :show false})
+
+  ;; TODO: support a book with multiple chapters
+  ;; Quarto: create 1 book (this is just our normal configuration, but Clay can help)
+  (make! {:format [:quarto :book]
+          ;; Clay: create 3 markdown files from source
+          :source-path ["notebooks/index.clj"
+                        "notebooks/index1.clj"
+                        "notebooks/index2.clj"]
+          :show false})
+
+  (make! {:format      [:html]
+          :source-path "notebooks/index.clj"
+          :single-form '(kind/cytoscape
+                         [{:style {:width "300px"
+                                   :height "300px"}}
+                          cytoscape-example])})
+
+  (make! {:format [:quarto :html]
+          :source-path "notebooks/index.clj"})
+
+  (make! {:format [:quarto :html]
+          :source-path "notebooks/slides.clj"})
+
+  (make! {:format [:quarto :revealjs]
+          :source-path "notebooks/slides.clj"})
+
+  (make! {:format [:quarto :html]
+          :source-path "notebooks/index.clj"
+          :quarto {:highlight-style :nord}}))
