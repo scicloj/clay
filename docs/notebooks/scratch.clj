70224d8b31ecfc60a54afad429e5dc9776e4fed0
daslu <daniel.slutsky@gmail.com>
2023-11-29 17:12:44 +0200

    rerendered tutorial after the new mechanism for js & css inclusion

--- docs/notebooks/scratch.clj
+++ docs/notebooks/scratch.clj
@@ -1,8 +1,19 @@
-(ns scratch
+(ns slides
   (:require [scicloj.kindly.v4.kind :as kind]
             [scicloj.clay.v2.api :as clay]
             [clojure.java.io :as io]
-            [clojure.java.shell :as shell]))
+            [clojure.java.shell :as shell]
+            [tablecloth.api :as tc]))
 
 (kind/hiccup
- [:img {:src "src/images/Clay.svg.png"}])
+ [:img {:src "notebooks/images/Clay.svg.png"}])
+
+;; # A
+
+;; ## A1
+
+(+ 1 2)
+
+;; ## A2
+
+;; # B
