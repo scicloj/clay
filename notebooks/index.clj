672db0a73068203dcf89860137357c1dbd7e79b7
daslu <daniel.slutsky@gmail.com>
2023-12-18 22:58:12 +0200

    moved the Emacs setup documentation to clay.el's README

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -75,29 +75,9 @@
 
 ;; ### Emacs CIDER
 ;;
-;; Please load the [clay.el](https://github.com/scicloj/clay.el) package.
-;;
-;; Using [use-package](https://github.com/jwiegley/use-package), you may do this as follows:
-;;
-;; ```elisp
-;; (use-package clay
-;; :straight (clay
-;;            :type git
-;;            :host github
-;;            :repo "scicloj/clay.el"))
-;; ```
-;;
-;; This package offers the following functions, that you may wish to create keybindings for:
-;;
-;; |name|function|
-;; |--|--|
-;; |`clay/start`|start clay if not started yet|
-;; |`clay/make-ns-hmtl`|save clj buffer, render it as html, and show that in the browser view|
-;; |`clay/make-ns-quarto-html`|save clj buffer, render it as quarto, render that as html, and show that in the browser view|
-;; |`clay/make-ns-quarto-revealjs`|save clj buffer, render it as quarto, render that as a revealjs slideshow, and show that in the browser view|
-;; |`clay/make-last-sexp`|render the last s-expression|
-;; |`clay/make-defun-at-point`|render the [defun-at-point](https://www.emacswiki.org/emacs/ThingAtPoint)|
+;; See the [clay.el](https://github.com/scicloj/clay.el) package for the relevant interactive functions.
 
+;;
 ;; ### IntelliJ Cursive
 ;; **(coming soon)**
 
