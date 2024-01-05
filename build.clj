e4b5ff93fd8930326616fa91360ec592a9361149
daslu <daniel.slutsky@gmail.com>
2024-01-05 08:19:21 +0200

    updated build setup (to support license in pom)

--- build.clj
+++ build.clj
@@ -1,29 +1,69 @@
 (ns build
+  "Kindly's build script.
+
+  clojure -T:build ci
+  clojure -T:build deploy
+
+  Run tests via:
+  clojure -X:test
+
+  For more information, run:
+
+  clojure -A:deps -T:build help/doc"
   (:refer-clojure :exclude [test])
-  (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
-            [org.corfield.build :as bb]))
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'org.scicloj/clay)
 (def version "2-alpha56")
-#_ ; alternatively, use MAJOR.MINOR.COMMITS:
-(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def snapshot (str version "-SNAPSHOT"))
+(def class-dir "target/classes")
 
-(defn test "Run the tests." [opts]
-  (bb/run-tests opts))
+(defn test "Run all the tests." [opts]
+  (doseq [alias [:1.10 :1.11 :master]]
+    (println "\nRunning tests for Clojure" (name alias))
+    (let [basis    (b/create-basis {:aliases [:test alias]})
+          cmds     (b/java-command
+                    {:basis     basis
+                     :main      'clojure.main
+                     :main-args ["-m" "cognitect.test-runner"]})
+          {:keys [exit]} (b/process cmds)]
+      (when-not (zero? exit) (throw (ex-info "Tests failed" {})))))
+  opts)
+
+(defn- pom-template [version]
+  [[:description "Clay is a small Clojure tool for a dynamic workflow of data visualization and literate programming."]
+   [:url "https://scicloj.github.io/clay/"]
+   [:licenses
+    [:license
+     [:name "Eclipse Public License - v 2.0"]
+     [:url "https://www.eclipse.org/legal/epl-2.0/"]]]])
+
+(defn- jar-opts [opts]
+  (let [version (if (:snapshot opts) snapshot version)]
+    (assoc opts
+           :lib lib   :version version
+           :jar-file  (format "target/%s-%s.jar" lib version)
+           :basis     (b/create-basis {})
+           :class-dir class-dir
+           :target    "target"
+           :src-dirs  ["src"]
+           :pom-data  (pom-template version))))
 
 (defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/run-tests)
-      (bb/clean)
-      (bb/jar)))
-
-(defn install "Install the JAR locally." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/install)))
+  (test opts)
+  (b/delete {:path "target"})
+  (let [opts (jar-opts opts)]
+    (println "\nWriting pom.xml...")
+    (b/write-pom opts)
+    (println "\nCopying source...")
+    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
+    (println "\nBuilding" (:jar-file opts) "...")
+    (b/jar opts))
+  opts)
 
 (defn deploy "Deploy the JAR to Clojars." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/deploy)))
+  (let [{:keys [jar-file] :as opts} (jar-opts opts)]
+    (dd/deploy {:installer :remote :artifact (b/resolve-path jar-file)
+                :pom-file (b/pom-path (select-keys opts [:lib :class-dir]))}))
+  opts)
