0a64ef833d4491711b63c42c6473d8794e45cca8
daslu <daniel.slutsky@gmail.com>
2024-01-16 23:28:07 +0200

    version "2-alpha66"

--- build.clj
+++ build.clj
@@ -15,7 +15,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'org.scicloj/clay)
-(def version "2-alpha65")
+(def version "2-alpha66")
 
 (def snapshot (str version "-SNAPSHOT"))
 (def class-dir "target/classes")
