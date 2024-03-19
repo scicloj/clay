eb2f3f3f8342c3dd21c3430caa6e7dbf34bb759b
adham-omran <git@adham-omran.com>
2024-03-19 18:13:33 +0300

    Add Highcharts line chart example

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -760,6 +760,46 @@ nested-structure-1
                   {:width "300px"
                    :height "300px"}}))
 
+;; ### Highcharts
+
+(kind/highcharts
+ {:title {:text "Line chart"}
+  :subtitle {:text "By Job Category"}
+  :yAxis {:title {:text "Number of Employees"}}
+  :series [{:name "Installation & Developers"
+            :data [43934, 48656, 65165, 81827, 112143, 142383,
+                   171533, 165174, 155157, 161454, 154610]}
+
+           {:name "Manufacturing",
+            :data [24916, 37941, 29742, 29851, 32490, 30282,
+                   38121, 36885, 33726, 34243, 31050]}
+
+           {:name "Sales & Distribution",
+            :data [11744, 30000, 16005, 19771, 20185, 24377,
+                   32147, 30912, 29243, 29213, 25663]}
+
+           {:name "Operations & Maintenance",
+            :data [nil, nil, nil, nil, nil, nil, nil,
+                   nil, 11164, 11218, 10077]}
+
+           {:name "Other",
+            :data [21908, 5548, 8105, 11248, 8989, 11816, 18274,
+                   17300, 13053, 11906, 10073]}]
+
+  :xAxis {:accessibility {:rangeDescription "Range: 2010 to 2020"}}
+
+  :legend {:layout "vertical",
+           :align "right",
+           :verticalAlign "middle"}
+
+  :plotOptions {:series {:label {:connectorAllowed false},
+                         :pointStart 2010}}
+
+  :responsive {:rules [{:condition {:maxWidth 500},
+                        :chartOptions {:legend {:layout "horizontal",
+                                                :align "center",
+                                                :verticalAlign "bottom"}}}]}})
+
 ;; ### Observable
 
 ;; [Observable](https://observablehq.com/) visualizations are supported
