81b8f167b400b378a32c53f00072c0d6590adfed
daslu <daniel.slutsky@gmail.com>
2024-10-27 17:32:04 +0200

    doc - extracted emmy-viewers as a separate chapter

--- notebooks/dev.clj
+++ notebooks/dev.clj
@@ -4,7 +4,8 @@
 (clay/make! {:format [:quarto :html]
              :base-source-path "notebooks"
              :source-path ["index.clj"
-                           "clay_book/examples.clj"]
+                           "clay_book/examples.clj"
+                           "clay_book/emmy_viewers.clj"]
              :base-target-path "docs"
              :book {:title "Clay Documentation"}
              :clean-up-target-dir true})
