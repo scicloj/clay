7cc1f77d42c0b814d631bea323a59718d6a815bc
Ram Narayan Kappagantula <57584367+SpacePirate27@users.noreply.github.com>
2024-12-31 12:14:39 +0530

    Add heading

--- notebooks/dummy.clj
+++ notebooks/dummy.clj
@@ -41,7 +41,21 @@
                                   (fn [response]
                                       (reset! *a1 response))))}]])])
 
-;; New example of a function that will calculate the open and click rates of the email data below
+(kind/md
+  "### New example of a function that will calculate the open and click rates of the email data below")
+
+(def email-data
+  [["email1@example.com" "19-12-2024 11:46:05" "19-12-2024 12:00:00" "19-12-2024 12:05:00"]
+   ["email2@example.com" "19-12-2024 11:46:06" "20-12-2024 12:00:00" nil]
+   ["email3@example.com" "19-12-2024 11:46:07" "21-12-2024 12:00:00" "21-12-2024 12:05:00"]
+   ["email4@example.com" "19-12-2024 11:46:08" "22-12-2024 12:00:00" "22-12-2024 12:05:00"]
+   ["email5@example.com" "19-12-2024 11:46:09" nil nil]
+   ["email6@example.com" "19-12-2024 11:46:10" "24-12-2024 12:00:00" "24-12-2024 12:05:00"]
+   ["email7@example.com" "19-12-2024 11:46:11" nil nil]
+   ["email8@example.com" "19-12-2024 11:46:12" "26-12-2024 12:00:00" "26-12-2024 12:05:00"]
+   ["email9@example.com" "19-12-2024 11:46:13" "27-12-2024 12:00:00" nil]
+   ["email10@example.com" "19-12-2024 11:46:14" "28-12-2024 12:00:00" nil]])
+
 (kind/reagent
   '(def email-data
      [["email1@example.com" "19-12-2024 11:46:05" "19-12-2024 12:00:00" "19-12-2024 12:05:00"]
