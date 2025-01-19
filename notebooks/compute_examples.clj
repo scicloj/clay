e4f3849d2b3889dace286c30ce4188cdad2e8142
daslu <daniel.slutsky@gmail.com>
2025-01-19 12:56:06 +0200

    updated compute_examples

--- notebooks/compute_examples.clj
+++ notebooks/compute_examples.clj
@@ -41,7 +41,7 @@
           [:input {:type     "button" :value "Click me!"
                    :on-click (fn []
                                (kindly-compute
-                                {:func 'dummy/add
+                                {:func 'compute-examples/add
                                  :args [@*a1 20]}
                                 (fn [response]
                                   (reset! *a1 response))))}]])))])
@@ -62,7 +62,12 @@
    ["email9@example.com" "19-12-2024 11:46:13" "27-12-2024 12:00:00" nil]
    ["email10@example.com" "19-12-2024 11:46:14" "28-12-2024 12:00:00" nil]])
 
-(kind/reagent
+(kind/table
+ {:column-names [:email :sent-at :opened-at :clicked-at]
+  :row-vectors  email-data})
+
+(kind/hiccup
+ [:div
   '(def email-data
      [["email1@example.com" "19-12-2024 11:46:05" "19-12-2024 12:00:00" "19-12-2024 12:05:00"]
       ["email2@example.com" "19-12-2024 11:46:06" "20-12-2024 12:00:00" nil]
@@ -73,24 +78,18 @@
       ["email7@example.com" "19-12-2024 11:46:11" nil nil]
       ["email8@example.com" "19-12-2024 11:46:12" "26-12-2024 12:00:00" "26-12-2024 12:05:00"]
       ["email9@example.com" "19-12-2024 11:46:13" "27-12-2024 12:00:00" nil]
-      ["email10@example.com" "19-12-2024 11:46:14" "28-12-2024 12:00:00" nil]]))
-
-(kind/table
-  {:column-names [:email :sent-at :opened-at :clicked-at]
-   :row-vectors  email-data})
-
-(kind/reagent
- ['(fn []
-     (let [*rates (reagent.core/atom {})]
-       (fn []
-         [:div
-          [:p (str "Open rate " (:open-rate @*rates))]
-          [:p (str "Click rate " (:click-rate @*rates))]
-          [:input {:type "button" :value "Click to calculate click and open rate"
-                   :on-click (fn []
-                               (kindly-compute
-                                {:func 'dummy/calc-click-and-open-rate
-                                 :args [email-data]}
-                                (fn [response]
-                                  (reset! *rates response))))}]])))])
+      ["email10@example.com" "19-12-2024 11:46:14" "28-12-2024 12:00:00" nil]])
+  ['(fn []
+      (let [*rates (reagent.core/atom {})]
+        (fn []
+          [:div
+           [:p (str "Open rate " (:open-rate @*rates))]
+           [:p (str "Click rate " (:click-rate @*rates))]
+           [:input {:type "button" :value "Click to calculate click and open rate"
+                    :on-click (fn []
+                                (kindly-compute
+                                 {:func 'compute-examples/calc-click-and-open-rate
+                                  :args [email-data]}
+                                 (fn [response]
+                                   (reset! *rates response))))}]])))]])
 
