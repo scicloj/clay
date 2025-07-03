404a47a8ad4c9fe9473adbd7c95d6eb2938e6c26
daslu <daniel.slutsky@gmail.com>
2025-07-03 08:30:33 +0300

    updated example

---
+++ notebooks/example.clj
@@ -0,0 +1,13 @@
+(ns example
+  (:require [scicloj.kindly.v4.kind :as kind]))
+
+(defn f [x] (+ x 3))
+
+(range (f 2))
+
+(kind/test-last [(fn [v]
+                   (= (count v) 5))])
+
+(+ 5 4)
+
+(kind/test-last [= 9])
--- test/example_generated_test.clj
+++ test/example_generated_test.clj
@@ -5,22 +5,16 @@
   [clojure.test :refer [deftest is]]))
 
 
-(def var3_line8 (defn f [x] (+ x 11)))
+(def var1_line4 (defn f [x] (+ x 3)))
 
 
-(def var4_line11 (range (f 11)))
+(def var2_line6 (range (f 2)))
 
 
-(def var6_line20 (def d (delay (Thread/sleep 1000) (+ 4 5))))
+(deftest test3_line8 (is ((fn [v] (= (count v) 5)) var2_line6)))
 
 
-(def var8_line27 (delay (Thread/sleep 1000) (+ 4 5)))
+(def var4_line11 (+ 5 4))
 
 
-(def var10_line33 (comment (+ 1 2) (delay (Thread/sleep 1000) (+ 4 5))))
-
-
-(def var12_line42 (+ 4 5))
-
-
-(deftest test13_line44 (is (= var12_line42 9)))
+(deftest test5_line13 (is (= var4_line11 9)))
