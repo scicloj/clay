1dc98d0e66f11f80bb246255bd7eacda71eed779
daslu <daniel.slutsky@gmail.com>
2025-07-07 02:17:27 +0300

    added chapter to book

--- notebooks/dev.clj
+++ notebooks/dev.clj
@@ -5,6 +5,7 @@
              :base-source-path "notebooks"
              :source-path ["index.clj"
                            "clay_book/examples.clj"
+                           "clay_book/test_generation.clj"
                            #_"clay_book/emmy_viewers.clj"]
              :base-target-path "docs"
              :book {:title "Clay Documentation"}
