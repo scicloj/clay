ab7e626b3a5d405650153dd22936dab833734330
Timothy Pratley <timothypratley@gmail.com>
2025-09-13 13:35:02 -0700

    remove libpython-clj replace with shell interop

---
+++ src/scicloj/clay/v2/plotly_export.clj
@@ -0,0 +1,24 @@
+(ns scicloj.clay.v2.plotly-export
+  (:require [scicloj.kindly-render.shared.jso :as jso]
+               [clojure.java.shell :as shell]))
+
+(def python-script
+  "import sys, json, plotly.graph_objects as go
+payload = json.load(sys.stdin)
+data = payload.get('data', [])
+layout = payload.get('layout', {})
+config = payload.get('config', {})
+filename = payload['filename']
+fig = go.Figure(data=data, layout=layout, config=config)
+fig.write_image(filename)
+")
+
+(defn export-plot! [filename data layout config]
+  ;; TODO: should figure out why these invalid properties are on data
+  (let [data (update data 0 dissoc :r :fill :mode :theta :z :lon :lat :width)
+        layout (dissoc layout :automargin)
+        payload (jso/write-json-str {:data data
+                                     :layout layout
+                                     :config config
+                                     :filename filename})]
+    (shell/sh "python3" "-c" python-script :in payload)))
