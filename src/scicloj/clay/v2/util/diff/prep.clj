a7a0274db91c04e1fd7f7ba5267ecb67f7d4d9ff
onbreath <65270097+onbreath@users.noreply.github.com>
2025-12-01 00:59:32 +0100

    Added ReplacedValue type for diffing, wraps replaced value

--- src/scicloj/clay/v2/util/diff/prep.clj
+++ src/scicloj/clay/v2/util/diff/prep.clj
@@ -16,20 +16,49 @@
 (defn type-str [x]
   (-> x type pr-str))
 
+(defn describe-type [t]
+  (type-str t))
+
+(declare replaced-value=)
+
+(defprotocol PReplacedValue
+  (value? [this]))
+
+(deftype ReplacedValue [value value-type]
+  PReplacedValue
+  (value? [_]
+    (= (describe-type value) value-type))
+
+  Object
+  (equals [this other]
+    (or (not (value? this))
+        (replaced-value= this other)))
+  (hashCode [this]
+    (hash-combine (hash value-type)
+                  (if (value? this)
+                    (hash value)
+                    value)))
+  (toString [this]
+    (pr-str (.hashCode this)
+            value-type)))
+
+(defn replaced-value= [^ReplacedValue this other]
+  (and (instance? ReplacedValue this)
+       (instance? ReplacedValue other)
+       (= (.-value-type this) (.-value-type ^ReplacedValue other))
+       (= (.-value this) (.-value ^ReplacedValue other))))
+
 (defn diffable-type? [x]
   (let [x-type-str (type-str x)]
     (some (partial String/.startsWith x-type-str)
           ["java.util"
            "clojure.lang"])))
 
-(defn describe-type [t]
-  (type-str t))
-
 (defn replace-undiffable* [x]
   (cond (fn? x) ::fn
         (and (satisfies? DiffableBaseType x)
              (diffable-base-type? x)
-             (not (diffable-type? x))) (describe-type x)
+             (not (diffable-type? x))) (->ReplacedValue x (describe-type x))
         :else x))
 
 (defn replace-undiffable [notes]
