ff5a3183da1e9d1f6e92de19352c58b4c6cd7932
daslu <daniel.slutsky@gmail.com>
2026-08-03 14:41:39 +0200

    version "2.0.20"

--- build.clj
+++ build.clj
@@ -15,7 +15,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'org.scicloj/clay)
-(def version "2.0.19")
+(def version "2.0.20")
 
 (def snapshot (str version "-SNAPSHOT"))
 (def class-dir "target/classes")
